3362e2f90035f8c458c08bb1ace216d78c75f11d
Liz Jaluague <e.jaluague@gmail.com>
2020-07-17 18:04:37 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chcolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray (array, element) {
-  var newArray = [];
-  array.unshift(element);
+  var newArray = array;
+  newArray.unshift(element);
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
