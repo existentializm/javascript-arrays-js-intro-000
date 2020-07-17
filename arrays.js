c74a3a4d6f8c7a0400ba45dbd48889fec19151e0
Liz Jaluague <e.jaluague@gmail.com>
2020-07-17 18:03:30 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chcolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray (array, element) {
+  var newArray = [];
+  array.unshift(element);
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element);
+}
